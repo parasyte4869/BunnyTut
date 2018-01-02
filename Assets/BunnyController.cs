b41f8791629f0968fc7d271842269423c4f292be
parasyte4869 <35031107+parasyte4869@users.noreply.github.com>
2018-01-02 19:54:42 +0700

    First use

---
+++ Assets/BunnyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyController : MonoBehaviour {
+
+    private Rigidbody2D myrb;
+    public float bunnyJumpForce = 500f;
+
+	// Use this for initialization
+	void Start () {
+        myrb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            myrb.AddForce(transform.up * bunnyJumpForce);
+        }
+	}
+}
